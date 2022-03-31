f0b94ba1fbb6171ee9ee4f1460a86213413f2b71
Saphira Kai <kai.saphira@gmail.com>
2022-03-31 03:25:35 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+[ $USER != root ] && {
+    echo "must be run as root" 1>&2
+    exit 1
+}
+
+install -Dvm 755 dcboost         ${DESTDIR:-}${PREFIX:-/usr}/bin/dcboost
+install -Dvm 755 dcboost.service ${DESTDIR:-}/etc/systemd/system/dcboost.service
+
+echo 'installed successfully'
